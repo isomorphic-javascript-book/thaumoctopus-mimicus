224a62929b88cf13dc11517891883d9dad90a31f
Jason Strimpel <jstrimpel@gmail.com>
2015-09-13 16:48:34 -0700

    editing updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var nodemon = require('gulp-nodemon');
+var sequence = require('run-sequence');
 
 gulp.task('compile', function () {
   return gulp.src('src/**/*.js')
@@ -21,4 +22,6 @@ gulp.task('start', function () {
   });
 });
 
-gulp.task('default', ['watch', 'compile', 'start']);
+gulp.task('default', function (callback) {
+  sequence(['compile', 'watch'], 'start', callback);
+});
