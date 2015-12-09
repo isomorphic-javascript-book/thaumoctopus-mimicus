fae1d0c277facf3befd684899be74d76b6ec1a9b
Jason Strimpel <jstrimpel@gmail.com>
2015-12-08 18:17:06 -0800

    redirect fix

--- src/HomeController.js
+++ src/HomeController.js
@@ -3,8 +3,8 @@ import Controller from './lib/Controller';
 export default class HomeController extends Controller {
 
   index(application, request, reply, callback) {
-    if (this.context.cookie.get('greeting')) {
-      return callback(null);
+    if (!this.context.cookie.get('greeting')) {
+      this.context.cookie.set('greeting', '1', { expires: 1000 * 60 * 60 * 24 * 365 });
     }
 
     this.context.cookie.set('greeting', '1', { expires: 1000 * 60 * 60 * 24 * 365 });
